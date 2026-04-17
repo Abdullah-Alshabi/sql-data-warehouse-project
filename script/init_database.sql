df5b502586f582331bf33263d6c37f285f3b708a
Abdullah Alshabi <139485834+Abdullah-Alshabi@users.noreply.github.com>
2026-04-17 15:20:02 +0300

    Update init_database.sql

--- script/init_database.sql
+++ script/init_database.sql
@@ -1,1 +1,26 @@
+-- Create Database 'DataWaregouse
+USE master;
+GO
+IF EXISTS (SELECT ! FROM sys.databases WHERE name = 'DataWarehouse')
+BEGIN
+	ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+	DROP DATABASE DataWarehouse;
+	END;
+
+CREATE DATABASE DataWarehouse;
+GO
+
+USE DataWarehouse;
+
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+
+
+
+
 
